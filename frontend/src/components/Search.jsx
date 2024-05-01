c950cfaa30dbb884af2102b3b9594108af93d8fe
YaroslavLeyman <yaroslavleiman@yandex.ru>
2024-05-01 11:44:35 +0700

    search user and sort repos

--- frontend/src/components/Search.jsx
+++ frontend/src/components/Search.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { IoSearch } from "react-icons/io5";
 
-const Search = () => {
-    return (
-		<form className='max-w-xl mx-auto p-2 sm:w-72'>
+const Search = ({ onSearch }) => {
+	const [username, setUsername] = useState("");
+
+	return (
+		<form className='max-w-xl mx-auto p-2' onSubmit={(e) => onSearch(e, username)}>
 			<label htmlFor='default-search' className='mb-2 text-sm font-medium text-gray-900 sr-only'>
 				Search
 			</label>
@@ -15,8 +17,10 @@ const Search = () => {
 					type='search'
 					id='default-search'
 					className='block w-full p-4 ps-10 text-sm rounded-lg bg-glass focus:ring-blue-500 focus:border-blue-500 bg-transparent focus:bg-transparent '
-					placeholder='i.e. johndoe'
+					placeholder='johndoe'
 					required
+					value={username}
+					onChange={(e) => setUsername(e.target.value)}
 				/>
 				<button
 					type='submit'
@@ -27,6 +31,6 @@ const Search = () => {
 			</div>
 		</form>
 	);
-}
+};
 
-export default Search
+export default Search;
--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -14,11 +14,11 @@ const HomePage = () => {
 
   const [sortType, setSortType] = useState("recent");
 
-  const getUserProfileAndRepos = useCallback(async () => {
+  const getUserProfileAndRepos = useCallback(async (username = "YaroslavLeyman") => {
     setLoading(true);
     try {
       const userRes = await fetch(
-        "https://api.github.com/users/YaroslavLeyman"
+        `https://api.github.com/users/${username}`
       );
       const userProfile = await userRes.json();
       setUserProfile(userProfile);
@@ -26,8 +26,8 @@ const HomePage = () => {
       const repoRes = await fetch(userProfile.repos_url);
       const repos = await repoRes.json();
       setRepos(repos);
-      console.log("userProfile", userProfile);
-      console.log("repos", repos);
+      
+      return { userProfile, repos };
     } catch (error) {
       toast.error(error.message);
     } finally {
@@ -38,6 +38,33 @@ const HomePage = () => {
   useEffect(() => {
     getUserProfileAndRepos();
   }, [getUserProfileAndRepos]);
+
+  const onSearch = async (e, username) => {
+		e.preventDefault();
+
+		setLoading(true);
+		setRepos([]);
+		setUserProfile(null);
+
+		const { userProfile, repos } = await getUserProfileAndRepos(username);
+
+		setUserProfile(userProfile);
+		setRepos(repos);
+		setLoading(false);
+		setSortType("recent");
+	};
+
+  const onSort = (sortType) => {
+		if (sortType === "recent") {
+			repos.sort((a, b) => new Date(b.created_at) - new Date(a.created_at)); //descending, recent first
+		} else if (sortType === "stars") {
+			repos.sort((a, b) => b.stargazers_count - a.stargazers_count); //descending, most stars first
+		} else if (sortType === "forks") {
+			repos.sort((a, b) => b.forks_count - a.forks_count); //descending, most forks first
+		}
+		setSortType(sortType);
+		setRepos([...repos]);
+	};
 
   return (
     <div className="m-4">

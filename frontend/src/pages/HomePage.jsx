5373717d670957d46b9fef22e3f84cc1a9007072
YaroslavLeyman <yaroslavleiman@yandex.ru>
2024-05-01 11:12:27 +0700

    got user profile and repos

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,22 +1,56 @@
-import React from 'react'
-import ProfileInfo from '../components/ProfileInfo';
-import Repos from '../components/Repos';
-import Search from '../components/Search';
-import SortRepos from '../components/SortRepos';
-import Spinner from '../components/Spinner';
+import React, { useCallback, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+
+import ProfileInfo from "../components/ProfileInfo";
+import Repos from "../components/Repos";
+import Search from "../components/Search";
+import SortRepos from "../components/SortRepos";
+import Spinner from "../components/Spinner";
 
 const HomePage = () => {
-  return (
-		<div className='m-4'>
-			<Search />
-			<SortRepos />
-			<div className='flex gap-4 flex-col lg:flex-row justify-center items-start'>
-				<ProfileInfo />
-				<Repos />
-				{/* <Spinner /> */}
-			</div>
-		</div>
-	);
-}
+  const [userProfile, setUserProfile] = useState(null);
+  const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
 
-export default HomePage
+  const [sortType, setSortType] = useState("recent");
+
+  const getUserProfileAndRepos = useCallback(async () => {
+    setLoading(true);
+    try {
+      const userRes = await fetch(
+        "https://api.github.com/users/YaroslavLeyman"
+      );
+      const userProfile = await userRes.json();
+      setUserProfile(userProfile);
+
+      const repoRes = await fetch(userProfile.repos_url);
+      const repos = await repoRes.json();
+      setRepos(repos);
+      console.log("userProfile", userProfile);
+      console.log("repos", repos);
+    } catch (error) {
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
+    }
+  }, []);
+
+  useEffect(() => {
+    getUserProfileAndRepos();
+  }, [getUserProfileAndRepos]);
+
+  return (
+    <div className="m-4">
+      <Search onSearch={onSearch} />
+      {repos.length > 0 && <SortRepos onSort={onSort} sortType={sortType} />}
+      <div className="flex gap-4 flex-col lg:flex-row justify-center items-start">
+        {userProfile && !loading && <ProfileInfo userProfile={userProfile} />}
+
+        {!loading && <Repos repos={repos} />}
+        {loading && <Spinner />}
+      </div>
+    </div>
+  );
+};
+
+export default HomePage;
